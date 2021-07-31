c7ca575eb8536b08efae487bab304fece181d151
traveller-42 <siamul.islam@northsouth.edu>
2021-07-31 15:53:37 +0600

    another page

---
+++ src/main/java/com/siam/AddServlet.java
@@ -0,0 +1,19 @@
+package com.siam;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+    public void service(HttpServletRequest req,HttpServletResponse resp) throws IOException {
+    	int i = Integer.parseInt(req.getParameter("num1"));
+    	int j = Integer.parseInt(req.getParameter("num2"));
+    	int k =i+j;
+    	//System.out.println("Result is ="+k);
+    	PrintWriter out= resp.getWriter();
+    	out.println("Result is ="+k);
+    }
+}
