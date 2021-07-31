e42b7facbb9242ce28cdc056a771070b2c32d053
traveller-42 <siamul.islam@northsouth.edu>
2021-07-31 18:04:44 +0600

    added Session as well

--- src/main/java/com/siam/AddServlet.java
+++ src/main/java/com/siam/AddServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
     public void doGet(HttpServletRequest req,HttpServletResponse resp) throws IOException, ServletException {
@@ -15,11 +16,12 @@ public class AddServlet extends HttpServlet {
     	int j = Integer.parseInt(req.getParameter("num2"));
     	int k =i+j;
     	//System.out.println("Result is ="+k);
-    	
+    	HttpSession session=req.getSession();
+    	session.setAttribute("k",k);
     	
 //    	req.setAttribute("k",k);
 //    	RequestDispatcher rd=req.getRequestDispatcher("ano");
 //    	rd.forward(req, resp);
-    	resp.sendRedirect("ano?k="+k);
+    	resp.sendRedirect("ano");
     }
 }
--- src/main/java/com/siam/SquareServlet.java
+++ src/main/java/com/siam/SquareServlet.java
@@ -7,10 +7,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SquareServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req,HttpServletResponse resp) throws IOException, ServletException  {
-		int number = Integer.parseInt(req.getParameter("k"));
+		HttpSession session=req.getSession();
+		int number = (int)session.getAttribute("k");
 		number=number*number;
 		PrintWriter out= resp.getWriter();
 		out.println("Square is ="+number);
